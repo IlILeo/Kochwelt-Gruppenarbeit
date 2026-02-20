575f4f2cc9465f4ab1db15f31d76ec76153a5d75
Ibrahim <duran.ibrahim998@gmail.com>
2026-02-20 19:12:25 +0100

    Rezepte und Kontaktseite gegenseitig ergänzt

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function berechneMengen() {
   zutaten.forEach(li => {
     const basisMenge = parseFloat(li.getAttribute('data-menge'));
     const einheit = li.getAttribute('data-einheit');
-    
+
     // Die Zutat finden (alles Text nach der ursprünglichen Zahl und Einheit)
     // Wir nehmen hier den ursprünglichen Textinhalt und säubern ihn
     const zutatName = li.textContent.split(einheit)[1] || "";
@@ -29,5 +29,22 @@ function berechneMengen() {
 
     // Inhalt aktualisieren
     li.innerHTML = `${neueMenge} ${einheit} ${zutatName.trim()}`;
+  });
+}
+
+function sendMail(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+
+  fetch("https://formspree.io/f/xeelzyvg", {
+    method: "POST",
+    body: new FormData(event.target),
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(() => {
+    window.location.href = "./send_mail.html";
+  }).catch((error) => {
+    console.log(error);
   });
 }
